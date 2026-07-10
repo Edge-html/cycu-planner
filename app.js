2b7eb492458f25f49bb7e88beaee96e59b741cd6
Edge-html <solanaian53@gmail.com>
2026-07-10 15:09:23 +0800

    Switch tiles to Google Maps road layer in English

--- app.js
+++ app.js
@@ -884,9 +884,9 @@ document.addEventListener('DOMContentLoaded', () => {
         attributionControl: false
       });
 
-      L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+      L.tileLayer('https://mt1.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}', {
+        maxZoom: 21,
+        attribution: '&copy; Google Maps'
       }).addTo(pinMap);
 
       if (pendingLat && pendingLng) {
@@ -943,9 +943,9 @@ document.addEventListener('DOMContentLoaded', () => {
       attributionControl: false
     });
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+    L.tileLayer('https://mt1.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}', {
+      maxZoom: 21,
+      attribution: '&copy; Google Maps'
     }).addTo(map);
 
     window.addEventListener('resize', () => {
